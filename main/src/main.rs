906ecfcf89359495aaddc099843103e3fa24631b
10DavidFranco <147568740+10DavidFranco@users.noreply.github.com>
2024-12-10 01:36:14 +0000

    Added error checker

--- main/src/main.rs
+++ main/src/main.rs
@@ -4,7 +4,7 @@ use std::time::Instant;
 fn main() {
    
     let now = Instant::now();
-   let url = "https://www.google.com/";
+   let url = "https://www.youtube.com/";
    let res: ureq::Response = ureq::get(url).call().unwrap();
    println!(
         "HTTP GET, no path interpolation, no query parameters:\n- URL: {}\n- res code: {},\n",
@@ -15,7 +15,11 @@ fn main() {
    );
    let elapsed = now.elapsed();
    println!("Reponse time: {:.2?}", elapsed);
-   
+   if res.status() == 200 {
+        println!("No errors encountered!");
+   }else{
+        println!("ERROR");
+   }
    
     //println!("Hello, world!");
 }
