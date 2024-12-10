34aefa3f060f70d772a4ee2d9bc9b0fa0d16dce3
10DavidFranco <147568740+10DavidFranco@users.noreply.github.com>
2024-12-10 17:41:32 +0000

    Second sesion

--- main/src/main.rs
+++ main/src/main.rs
@@ -1,12 +1,23 @@
 use ureq;
 use std::time::Instant;
+use std::thread;
 
 fn main() {
-   
+   let totaltime = Instant::now();
 
-   let urls: Vec<String> = vec!["https://www.youtube.com/".to_string(),"https://www.google.com/".to_string(), "https://www.chess.com/home".to_string()];
+   let urls: Vec<String> = vec![
+     "https://www.youtube.com/".to_string(),
+     "https://www.google.com/".to_string(), 
+     "https://www.chess.com/home".to_string(),
+     "https://leetcode.com/".to_string(),
+     ];
 
    for url in urls{
+
+
+   let handle = thread::spawn(move || { 
+
+
    let now = Instant::now();
    //let url = "https://www.youtube.com/";
    let res: ureq::Response = ureq::get(&url).call().unwrap();
@@ -18,12 +29,22 @@ fn main() {
         
    );
    let elapsed = now.elapsed();
-   println!("Reponse time: {:.2?}", elapsed);
+   println!("Response time: {:.2?}", elapsed);
    if res.status() == 200 {
         println!("No errors encountered!\n\n");
    }else{
         println!("ERROR\n\n");
    }
+
+
+   });
+
+   handle.join().unwrap();
 }
+
+
+
+let totalelapsed = totaltime.elapsed();
+println!("Total time taken: {:.2?}", totalelapsed);
     //println!("Hello, world!");
 }
