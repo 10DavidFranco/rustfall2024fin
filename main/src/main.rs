ce81a645a490560f9e6e28b125d1ed92532a2070
10DavidFranco <147568740+10DavidFranco@users.noreply.github.com>
2024-12-10 19:06:16 +0000

    main simplification

--- main/src/main.rs
+++ main/src/main.rs
@@ -3,19 +3,8 @@ use std::time::Instant;
 use std::thread;
 use std::time::Duration;
 
-fn main() {
+fn webchecker(urls: Vec<String>){
     let totaltime = Instant::now();
-
-    let urls: Vec<String> = vec![
-        "https://www.youtube.com/".to_string(),
-        "https://www.google.com/".to_string(),
-        "https://www.chess.com/home".to_string(),
-        "https://leetcode.com/".to_string(),
-        "https://www.coolmathgames.com/".to_string(),
-        "https://www.utrgv.edu/".to_string(),
-     
-
-    ];
 
     let mut handles = vec![];
 
@@ -46,7 +35,7 @@ fn main() {
                 }
                 Err(e) => {
                    
-                    println!("!!!!!Failed to request URL {}: {}!!!!!\n\n", url, e);
+                    println!("\n\n!!!!!Failed to request URL {}: {}!!!!!\n\n", url, e);
                 }
             }
         });
@@ -54,11 +43,70 @@ fn main() {
         handles.push(handle);
     }
 
- 
     for handle in handles {
         handle.join().unwrap();
     }
 
     let totalelapsed = totaltime.elapsed();
     println!("Total time taken: {:.2?}", totalelapsed);
+}
+
+fn main() {
+    //let totaltime = Instant::now();
+
+    let urls: Vec<String> = vec![
+        "https://www.youtube.com/".to_string(),
+        "https://www.google.com/".to_string(),
+        "https://www.chess.com/home".to_string(),
+        "https://leetcode.com/".to_string(),
+        "https://www.coolmathgames.com/".to_string(),
+        "https://www.utrgv.edu/".to_string(),
+     
+
+    ];
+
+    webchecker(urls);
+    // let mut handles = vec![];
+
+    // for url in urls {
+    //     let handle = thread::spawn(move || {
+    //         let now = Instant::now();
+
+            
+    //         let res = ureq::get(&url).timeout(Duration::new(5, 0)).call();
+
+    //         match res {
+    //             Ok(response) => {
+                    
+    //                 println!(
+    //                     "HTTP GET, no path interpolation, no query parameters:\n- URL: {}\n- res code: {}",
+    //                     url,
+    //                     response.status()
+    //                 );
+
+    //                 let elapsed = now.elapsed();
+    //                 println!("Response time for {}: {:.2?}", url, elapsed);
+
+    //                 if response.status() == 200 {
+    //                     println!("No errors encountered!\n\n");
+    //                 } else {
+    //                     println!("ERROR with status code: {}\n\n", response.status());
+    //                 }
+    //             }
+    //             Err(e) => {
+                   
+    //                 println!("\n\n!!!!!Failed to request URL {}: {}!!!!!\n\n", url, e);
+    //             }
+    //         }
+    //     });
+
+    //     handles.push(handle);
+    // }
+
+    // for handle in handles {
+    //     handle.join().unwrap();
+    // }
+
+    // let totalelapsed = totaltime.elapsed();
+    // println!("Total time taken: {:.2?}", totalelapsed);
 }
